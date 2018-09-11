69fca917bb80ee2bb36652ea109c0fd8fd5178c2
ToeiKanta <41673034+ToeiKanta@users.noreply.github.com>
2018-09-11 16:33:43 +0700

    Update draw2.cpp

--- draw2.cpp
+++ draw2.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
-			n=17
+            n=17
       12345678901234567
 1     X-------+-------X
 2     |\      |      /|
@@ -24,30 +24,39 @@
 int main(){
 	int n=17,count=0;
 	printf("%-6c",' ');
-	
-	for(int i=1;count<n;i++){
+	for(int i = 1; count < n ; i++){
 		count++;
-		if(i==10) i=0;
+		if(i==10) 
+			i=0;
 		printf("%d",i);
 	}
 	printf("\n");
-	for(int r =1;r<=n;r++){
+	for(int r = 1 ; r <= n ; r++){
 		printf("%-5d ",r);
-		for(int c =1;c<=n;c++){
-			if(r==1 || r ==n){
-				if(c==1 || c==n) printf("X");else
-				if(c == n/2+1) printf("+");else 
-				printf("-");
+		for(int c = 1 ; c <= n ; c++){
+			if(r == 1 || r == n){
+				if(c==1 || c==n) printf("X");
+				else
+					if(c == n/2+1) printf("+");
+				else 
+					printf("-");
 			}else
-			if(r==n/2+1){
-				if(c==1 || c==n || c==r) printf("+");else
-				printf("-");
-			}
+				if(r == n/2+1){
+					if(c == 1 || c == n || c == r) 
+						printf("+");
+					else
+						printf("-");
+			}else
+				if(c == 1 || c == n || c == n/2+1) 
+					printf("|");
 			else
-			if(c==1 || c==n || c== n/2+1) printf("|");else
-			if((c<n/2+1 && r<n/2+1 && c==r) || (c>=n/2+1 && r>=n/2+1 && r==c)) printf("\\"); else
-			if((c<=n/2+1 && r>=n/2+1 && c==n-r+1) || (c>=n/2+1 && r<n/2+1 && c==n-r+1)) printf("/");else
-			printf(" ");
+				if((c <= n/2+1 && r <= n/2+1 && c == r) || (c >= n/2+1 && r >= n/2+1 && r == c)) 
+					printf("\\"); 
+			else
+				if((c <= n/2+1 && r >= n/2+1 && c == n-r+1) || (c >= n/2+1 && r <= n/2+1 && c == n-r+1))
+					printf("/");
+			else
+				printf(" ");
 		}
 		printf("\n");
 	}
